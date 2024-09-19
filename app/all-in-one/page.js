d95a21d5d6a061303592cd9e494fae5075a1e7b0
Kaniska <kaniska.mitra@gmail.com>
2024-09-20 00:21:16 +0530

    Updated Projects section

--- app/all-in-one/page.js
+++ app/all-in-one/page.js
@@ -218,6 +218,25 @@ export default function Portfolio() {
               </div>
             </div>
           </div>
+          <div className="bg-white dark:bg-gray-800 bg-opacity-50 dark:bg-opacity-50 rounded-lg p-6 backdrop-filter backdrop-blur-lg border border-purple-500">
+            <div className="flex flex-col md:flex-row items-center">
+              <div className="md:w-1/3 mb-4 md:mb-0 md:mr-6">
+                <Image
+                  src="/Coordina.png?height=150&width=150"
+                  alt="AquaSolve - pHighters"
+                  width={300}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+              <div className="md:w-2/3">
+                <h4 className="text-xl font-semibold mb-2 font-object-sans">Coordina</h4>
+                <p className="font-montserrat mb-4">Coordina is a comprehensive urban management platform designed to transform India's cities for a smarter tomorrow. It offers tools for project management, resource allocation, conflict resolution, and interdepartmental collaboration.</p>
+                <p className="font-montserrat mb-4">Our team was placed among select few teams who qualified for the finals of the Smart India Hackathon.</p>
+                <a href="https://coordina-murex.vercel.app" target="_blank" rel="noopener noreferrer" className="text-purple-500 hover:text-purple-600 transition-colors font-montserrat">View Project</a>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
 
